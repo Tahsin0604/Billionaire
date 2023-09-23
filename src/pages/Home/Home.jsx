7409af152bee5af955a6e53f64e0158137408464
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2023-09-23 10:46:05 +0600

    loader added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchUsersList } from "../../redux/features/users/userSlice";
 import { FilterContext } from "../../context/Context";
 import { Helmet } from "react-helmet-async";
+import loader from "/loader.svg";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const usersList = useSelector((state) => state.userSlice.usersList);
+  const { usersList, usersLoading } = useSelector((state) => state.userSlice);
   const {
     filterState: { orderedBy, searchQuery },
   } = useContext(FilterContext);
@@ -33,6 +34,9 @@ const Home = () => {
       <Helmet>
         <title>Billionaire Profile | Home</title>
       </Helmet>
+      <div className="py-10 px-4 md:px-8 lg:px-36 flex justify-center w-full">
+        {usersLoading && <img src={loader} alt="" />}
+      </div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 py-10 px-4 md:px-10 ">
         {FilterdUserList().map((user) => (
           <UserCard key={user.naturalId} user={user} />
